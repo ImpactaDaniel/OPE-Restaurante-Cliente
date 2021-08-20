f15ed4b6fd661374346d136a7d1f6e2977f6c094
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-08-19 22:59:06 -0300

    Ajustando testes pra usarem mock quando o que for testado não é o repositório.

--- Restaurante.Test/ClienteTests/BuscarClienteRequisicaoHandlerTest.cs
+++ Restaurante.Test/ClienteTests/BuscarClienteRequisicaoHandlerTest.cs
@@ -2,9 +2,11 @@
 using Restaurante.Application.Usuarios.Clientes.Requsicoes.Buscar;
 using Restaurante.Domain.Comum.Modelos;
 using Restaurante.Domain.Comum.Modelos.Intefaces;
-using Restaurante.Infra.Usuarios.Clientes;
+using Restaurante.Domain.Usuarios.Modelos;
+using Restaurante.Domain.Usuarios.Repositorios;
 using Restaurante.Test.ClienteTests.Mock;
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 using static Restaurante.Application.Usuarios.Clientes.Requsicoes.Buscar.BuscarClientePorIdRequisicao;
@@ -15,11 +17,12 @@ namespace Restaurante.Test.ClienteTests
     {
 
         private readonly IClienteValidator _clienteValidator;
+        private readonly IClienteDomainRepositorio _repositorio;
 
         public BuscarClienteRequisicaoHandlerTest()
         {
             _clienteValidator = Substitute.For<IClienteValidator>();
-
+            _repositorio = Substitute.For<IClienteDomainRepositorio>();
             _clienteValidator.Validar(default).ReturnsForAnyArgs(new Resposta());
         }
 
@@ -29,17 +32,18 @@ namespace Restaurante.Test.ClienteTests
             //Arrange
             var cliente = await ClienteMock.GetClientePadrao();
 
-            var repositorio = await ClienteRepositorioMock.GetContextUsingInMemoryDBComClienteInserido(cliente, _clienteValidator);
+            _repositorio.Buscar(default).ReturnsForAnyArgs(new RespostaConsulta<Cliente>(await ClienteMock.GetClientePadrao()));
 
             var comandoBusca = new BuscarClientePorIdRequisicao() { Id = cliente.Id};
 
-            var handlerBuscar = new BuscarClientePorIdRequisicaoHandler(repositorio);
+            var handlerBuscar = new BuscarClientePorIdRequisicaoHandler(_repositorio);
 
             //Act
             var clienteBusca = await handlerBuscar.Handle(comandoBusca, new System.Threading.CancellationToken());
 
             //Assert
             Assert.True(clienteBusca.Sucesso);
+            Assert.Null(clienteBusca.Erros);
             Assert.NotNull(clienteBusca.Entidade);
         }
 
@@ -49,17 +53,20 @@ namespace Restaurante.Test.ClienteTests
             //Arrange
             var clientePadrao = ClienteMock.GetClientePadrao();
 
-            var repositorio = new ClienteRepositorio(ClienteRepositorioMock.GetDbContextPadraoUsingMemoryDatabase(Guid.NewGuid().ToString()), _clienteValidator);
+            var erros = new List<string>() { "Erro" };
+
+            _repositorio.Buscar(default).ReturnsForAnyArgs(new RespostaConsulta<Cliente>(erros));
 
             var comandoBusca = new BuscarClientePorIdRequisicao() { Id = clientePadrao.Id };
 
-            var handlerBuscar = new BuscarClientePorIdRequisicaoHandler(repositorio);
+            var handlerBuscar = new BuscarClientePorIdRequisicaoHandler(_repositorio);
 
             //Act
             var resposta = await handlerBuscar.Handle(comandoBusca, new System.Threading.CancellationToken());
 
             //Assert
             Assert.False(resposta.Sucesso);
+            Assert.True(resposta.Erros?.Any());
             Assert.Null(resposta.Entidade);
         }
     }
