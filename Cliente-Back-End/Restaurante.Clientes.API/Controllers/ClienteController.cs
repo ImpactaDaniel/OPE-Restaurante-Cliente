55260e6cf4b22a0367085a4bfdc222ce4215f38e
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-11-17 23:04:54 -0300

    Criando novo projeto web.

---
+++ Cliente-Back-End/Restaurante.Clientes.API/Controllers/ClienteController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Restaurante.Clientes.API.Controllers
+{
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        [("ItWorks")]
+        public IActionResult Index()
+        {
+            return Ok("ItWorks");
+        }
+    }
+}
