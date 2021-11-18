76fbbf8943fb4f59fd0dfba846a5e03d38ffc576
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-11-18 19:23:52 -0300

    Removendo referencias a bancos de dados.

--- Cliente-Back-End/Restaurante.Infra/ConfiguracaoInfra.cs
+++ Cliente-Back-End/Restaurante.Infra/ConfiguracaoInfra.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurante.Clientes.Infra.Usuarios.Encoder.Interfaces;
 using Restaurante.Domain.Usuarios.Modelos.Intefaces;
 using Restaurante.Domain.Usuarios.Repositorios.Interfaces;
-using Restaurante.Infra.Comum.Persistencia;
-using Restaurante.Infra.Usuarios.Clientes;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Restaurante.Clientes.Test")]
@@ -15,18 +12,9 @@ namespace Restaurante.Infra
     {
         public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration) =>
             services
-                .AddDatabase(configuration)
                 .AddValidators()
                 .AddEncoders()
                 .AddRepositorios();
-
-        private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration) =>
-            services
-                .AddDbContext<RestauranteDbContext>(options =>
-                    options.UseSqlite(configuration.GetConnectionString("Default"),
-                                            Sqlite => Sqlite
-                                                        .MigrationsAssembly(typeof(RestauranteDbContext).Assembly.FullName)))
-                .AddScoped<IRestauranteDbContext>(provider => provider.GetService<RestauranteDbContext>());
 
         internal static IServiceCollection AddValidators(this IServiceCollection services) =>
              services.Scan(scan => scan
--- Cliente-Back-End/Restaurante.Infra/Usuarios/Clientes/ClienteRepositorio.cs
+++ Cliente-Back-End/Restaurante.Infra/Usuarios/Clientes/ClienteRepositorio.cs
@@ -1,103 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Restaurante.Clientes.Infra.Usuarios.Encoder.Interfaces;
+﻿using Restaurante.Clientes.Infra.Usuarios.Encoder.Interfaces;
 using Restaurante.Domain.Comum.Modelos;
 using Restaurante.Domain.Comum.Modelos.Intefaces;
 using Restaurante.Domain.Usuarios.Modelos;
 using Restaurante.Domain.Usuarios.Repositorios.Interfaces;
-using Restaurante.Infra.Comum.Persistencia;
-using System.Collections.Generic;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Restaurante.Infra.Usuarios.Clientes
 {
     public class ClienteRepositorio :
-        RepositorioDados<IRestauranteDbContext, Cliente>,
         IClienteDomainRepositorio
     {
         private readonly IClienteValidator _clienteValidator;
         private readonly IPasswordEncoder _encoder;
-        public ClienteRepositorio(IRestauranteDbContext dbContext, IClienteValidator clienteValidator, IPasswordEncoder encoder) : base(dbContext)
+        public ClienteRepositorio(IClienteValidator clienteValidator, IPasswordEncoder encoder)
         {
             _encoder = encoder;
             _clienteValidator = clienteValidator;
         }
 
-        public override async Task<RespostaConsulta<Cliente>> Salvar(Cliente entidade, CancellationToken cancellationToken = default)
+        public Task<RespostaConsulta<Cliente>> Buscar(int id, CancellationToken cancellationToken = default)
         {
-            var respostaValidator = _clienteValidator.Validar(entidade);
-            if (!respostaValidator.Sucesso)
-                return new RespostaConsulta<Cliente>(respostaValidator.Erros);
-
-            entidade.AtualizarSenha(_encoder.Encode(entidade.Senha));
-            return await base.Salvar(entidade, cancellationToken);
+            throw new NotImplementedException();
         }
 
-        public async Task<RespostaConsulta<Cliente>> Buscar(int id, CancellationToken cancellationToken = default)
+        public RespostaConsulta<Cliente> Buscar(Func<Cliente, bool> condicao)
         {
-            var cliente = await
-                All()
-                .Include(c => c.Endereco)
-                .Include(c => c.Telefone)
-                .FirstOrDefaultAsync(c => c.Id == id);
-
-            if (cliente is null)
-                return RetornaErro<Cliente>("Cliente não encontrado!");
-            return new RespostaConsulta<Cliente>(cliente);
+            throw new NotImplementedException();
         }
 
-        public async Task<RespostaConsulta<Endereco>> BuscarEndereco(int id, CancellationToken cancellationToken = default)
+        public Task<RespostaConsulta<Endereco>> BuscarEndereco(int id, CancellationToken cancellationToken = default)
         {
-            var endereco = await Data
-                .Enderecos
-                .FirstOrDefaultAsync(e => e.Id == id);
-
-            if (endereco is null)
-                return RetornaErro<Endereco>("Endereço não encontrado!");
-
-            return new RespostaConsulta<Endereco>(endereco);
-        }
-        public async Task<RespostaConsulta<Telefone>> BuscarTelefone(int id, CancellationToken cancellationToken = default)
-        {
-            var erros = new List<string>(1);
-
-            var telefone = await
-                Data
-                .Telefones
-                .FirstOrDefaultAsync(t => t.Id == id);
-
-            if (telefone is null)
-                return RetornaErro<Telefone>("Telefone não encontrado!");
-            return new RespostaConsulta<Telefone>(telefone);
+            throw new NotImplementedException();
         }
 
-        public async Task<bool> Deletar(int id, CancellationToken cancellationToken = default)
+        public Task<RespostaConsulta<Telefone>> BuscarTelefone(int id, CancellationToken cancellationToken = default)
         {
-            var cliente = await Data.Clientes.FindAsync(id);
-
-            if (cliente is null)
-                return false;
-
-            Data.Clientes.Remove(cliente);
-
-            await Data.SaveChangesAsync(cancellationToken);
-
-            return true;
+            throw new NotImplementedException();
         }
 
-        public async Task<RespostaConsulta<Cliente>> Logar(string email, string password, CancellationToken cancellationToken = default)
+        public Task<bool> Deletar(int id, CancellationToken cancellationToken = default)
         {
-            var cliente = await Data.Clientes.FirstAsync(t => t.Email == email);
+            throw new NotImplementedException();
+        }
 
-            if (cliente != null)
-            {
-                if (_encoder.VerficarSenha(cliente.Senha, password))
-                    return RetornaErro<Cliente>("Cliente email e senha incorretos.");
+        public Task<RespostaConsulta<Cliente>> Logar(string email, string password, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-                return new RespostaConsulta<Cliente>(cliente);
-            }
-
-            return RetornaErro<Cliente>("Cliente não encontrado.");
+        public Task<RespostaConsulta<Cliente>> Salvar(Cliente entidade, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
         }
     }
 }
