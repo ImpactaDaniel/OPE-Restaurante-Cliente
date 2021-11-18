7878c254a13c8c47a332538e7d416dac71c017c8
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-11-17 22:50:32 -0300

    Separando front do back

--- Cliente-Back-End/Restaurante.Clientes.WebSite/Startup.cs
+++ Cliente-Back-End/Restaurante.Clientes.WebSite/Startup.cs
@@ -19,12 +19,7 @@ namespace Restaurante.Clientes.WebSite
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,19 +52,6 @@ namespace Restaurante.Clientes.WebSite
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
         }
     }
