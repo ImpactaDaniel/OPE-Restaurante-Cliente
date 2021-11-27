dfb4c89d192e35bbf503d5c4aa3543c0f371ab8c
ImpactaDaniel <daniel.dias@aluno.faculdadeimpacta.com.br>
2021-11-26 22:24:42 -0300

    Implementando repositorio de pedidos.

---
+++ Cliente-Back-End/Restaurante.Infra/Invoices/Repositories/InvoiceRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Restaurante.Clientes.Domain.Invoices.Interfaces;
+using Restaurante.Integrations;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Restaurante.Clientes.Infra.Invoices.Repositories
+{
+    public class InvoiceRepository : IInvoiceDomainRepository
+    {
+
+        private readonly RestauranteService _restauranteService;
+        private readonly IMapper _mapper;
+
+        public InvoiceRepository(RestauranteService restauranteService, IMapper mapper)
+        {
+            _restauranteService = restauranteService;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<Restaurante.Domain.Pedidos.Models.Invoice>> GetByCustomer(int customerId, CancellationToken cancellationToken = default)
+        {
+            var invoices = await _restauranteService
+                .GetByCystomerAsync(customerId, cancellationToken);
+
+            if (!invoices.Success)
+                return null;
+
+            return _mapper.Map<IEnumerable<Restaurante.Domain.Pedidos.Models.Invoice>>(invoices.Result);
+        }
+
+        public async Task<Restaurante.Domain.Pedidos.Models.Invoice> GetById(int id, CancellationToken cancellationToken = default)
+        {
+            var invoices = await _restauranteService
+                .Get4Async(id, cancellationToken);
+
+            if (!invoices.Success)
+                return null;
+
+            return _mapper.Map<Restaurante.Domain.Pedidos.Models.Invoice>(invoices.Result);
+        }
+    }
+}
